04bd61cb483973ccfad6dfe2791e58d4126eb629
Chris_tAt <christati1205@gmail.com>
2023-04-17 16:51:59 -0500

    aqui esta mi homework del modulo numero 5, fue muy dificil y me costo demasiado realizarlo, sin embargo despues de repasar y repasar logre hacer todos los ejericicios

---
+++ M05 JavaScript Arrays/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+function agregarItemAlComienzoDelArray(array, elemento) {
+   // Agrega el "elemento" al comienzo del arreglo recibido.
+   // Retorna el arreglo.
+   // Tu código:
+   array.unshift(elemento);
+   return array;
