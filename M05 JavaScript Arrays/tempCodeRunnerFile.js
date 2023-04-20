b44e2bdb69192388627f851606e687d957a2fe59
Chris_tAt <christati1205@gmail.com>
2023-04-20 16:47:18 -0500

    estos son los resultados del test 06 del homeword

--- M05 JavaScript Arrays/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-function agregarItemAlComienzoDelArray(array, elemento) {
-   // Agrega el "elemento" al comienzo del arreglo recibido.
-   // Retorna el arreglo.
-   // Tu código:
-   array.unshift(elemento);
-   return array;
